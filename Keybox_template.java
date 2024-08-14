6bd1a2dbd3af16e307d685f8549d00229c1489a0
thanhtv69 <133558156+thanhtv69@users.noreply.github.com>
2024-08-14 16:25:29 +0700

    Create Keybox_template.java

---
+++ Keybox_template.java
@@ -0,0 +1,36 @@
+package com.android.internal.util.framework;
+
+import org.lsposed.lsparanoid.Obfuscate;
+
+@Obfuscate
+public final class Keybox {
+    public static final class EC {
+        public static final String PRIVATE_KEY = """
+                PLACEHOLDER_EC_PRIVATE_KEY
+                """;
+        public static final String CERTIFICATE_1 = """
+                PLACEHOLDER_EC_CERTIFICATE_1
+                """;
+        public static final String CERTIFICATE_2 = """
+                PLACEHOLDER_EC_CERTIFICATE_2
+                """;
+        public static final String CERTIFICATE_3 = """
+                PLACEHOLDER_EC_CERTIFICATE_3
+                """;
+    }
+
+    public static final class RSA {
+        public static final String PRIVATE_KEY = """
+                PLACEHOLDER_RSA_PRIVATE_KEY
+                """;
+        public static final String CERTIFICATE_1 = """
+                PLACEHOLDER_RSA_CERTIFICATE_1
+                """;
+        public static final String CERTIFICATE_2 = """
+                PLACEHOLDER_RSA_CERTIFICATE_2
+                """;
+        public static final String CERTIFICATE_3 = """
+                PLACEHOLDER_RSA_CERTIFICATE_3
+                """;
+    }
+}
