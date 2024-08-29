ba883851aedb86779bd158468c7d180eae220763
thanhtv69 <133558156+thanhtv69@users.noreply.github.com>
2024-08-29 08:57:11 +0700

    Update Android.java

--- app/src/main/java/com/android/internal/util/framework/Android.java
+++ app/src/main/java/com/android/internal/util/framework/Android.java
@@ -44,7 +44,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
-@Obfuscate
+// @Obfuscate
 public final class Android {
     private static final String TAG = "chiteroman";
     private static final PEMKeyPair EC, RSA;
